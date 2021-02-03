cb82cf09effbbf0e46a98279fc4cdd15201ffb0b
David Carlin <david.carlin@cultureamp.com>
2021-02-04 10:58:16 +1100

    Switch to using useReducer and useEffect

--- src/App.js
+++ src/App.js
@@ -1,42 +1,89 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import Lights from './Lights.js';
-import './App.css';
+import React, { useReducer, useRef, useEffect } from "react";
+import Lights from "./Lights.js";
+import "./App.css";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "start":
+      const patt = [1, 2, 3, 4].sort(() => Math.random() - 0.5);
+      return { ...state, pattern: patt, patternToPlay: patt, gameState: "play" };
+    case "replay":
+      return { ...state, patternToPlay: state.pattern, gameState: "play" };
+    case "lightClick":
+      const newPattern = [...state.incomingPattern, action.number];
+      if (newPattern.slice(state.pattern.length * -1).join() === state.pattern.join()) {
+        return { ...state, incomingPattern: newPattern, gameState: "win" };
+      }
+      return { ...state, incomingPattern: newPattern };
+    case "playPattern":
+      if (state.patternToPlay.length > 0) {
+        return {
+          ...state,
+          light: state.patternToPlay[0],
+          patternToPlay: state.patternToPlay.slice(1),
+        };
+      } else {
+        clearTimeout(action.timeout);
+        return { ...state, light: null, gameState: "listen" };
+      }
+    default:
+      return state;
+  }
+};
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 function App() {
-  const [pattern, setPattern] = useState([321]);
-  const [incomingPattern, setIncomingPattern] = useState([]);
-  const [light, setLight] = useState(0);
-  const [gameState, setGameState] = useState("play");
+  const initialState = {
+    gameState: "play",
+    pattern: [321],
+    patternToPlay: [],
+    incomingPattern: [],
+    light: 0,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const start = () => {
-    const patt = [1,2,3,4].sort(() => Math.random() - 0.5);
-    setPattern(patt);
-    setTimeout(() => playPattern(patt), 1000);
-  }
+    setTimeout(() => tick(), 1000);
+    dispatch({ type: "start" });
+  };
 
-  const playPattern = (patt) => {
-    setLight(patt[0])
-    if (patt.length > 0) {
-      setTimeout(() => playPattern(patt.slice(1)), 1000)
-    } else {
-      setGameState("listen");
-    }
-  }
+  const replay = () => {
+    setTimeout(() => tick(), 1000);
+    dispatch({ type: "replay" });
+  };
 
-  const buttonClick = (i) => {
-    const newPattern = [...incomingPattern, i]
-    if (newPattern.slice(pattern.length * -1).join() === pattern.join()) {
-      alert("you win!");
+  const tick = () => {
+    var timeout = setTimeout(() => tick(), 1000);
+    dispatch({ type: "playPattern", timeout: timeout });
+  };
+
+  const prevGameState = usePrevious(state.gameState);
+
+  useEffect(() => {
+    if (prevGameState !== "win" && state.gameState === "win") {
+      alert("You Win");
     }
-    setIncomingPattern(newPattern);
-  }
+  });
 
   return (
     <div className="App">
-      <Lights up={light} gameState={gameState} click={buttonClick}></Lights>
+      <Lights
+        up={state.light}
+        gameState={state.gameState}
+        click={(i) => {
+          dispatch({ type: "lightClick", number: i });
+        }}
+      ></Lights>
       <button onClick={() => start()}>Start</button>
-      <button onClick={() => playPattern(pattern)}>Replay</button>
+      <button onClick={() => replay()}>Replay</button>
     </div>
   );
 }
